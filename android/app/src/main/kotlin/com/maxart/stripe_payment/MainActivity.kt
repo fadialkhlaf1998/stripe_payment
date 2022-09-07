ff56da2a52513461b6c68712468c1d24a2d20923
Fadi-h <62059908+Fadi-h@users.noreply.github.com>
2022-09-07 15:42:45 +0300

    f

--- android/app/src/main/kotlin/com/maxart/stripe_payment/MainActivity.kt
+++ android/app/src/main/kotlin/com/maxart/stripe_payment/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.maxart.stripe_payment
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+
+class MainActivity: FlutterFragmentActivity () {
+
 }
