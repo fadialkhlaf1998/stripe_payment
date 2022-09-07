c479f428b3b1f780ee9eb49bd89ebcdf786383e6
Fadi-h <62059908+Fadi-h@users.noreply.github.com>
2022-09-07 15:45:51 +0300

    f

---
+++ lib/controller/payment_controller.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class PaymentController extends GetxController {
+  Map<String, dynamic>? paymentIntentData;
+
+  Future<void> makePayment(
+      {required String amount, required String currency}) async {
+    try {
+      paymentIntentData = await createPaymentIntent(amount, currency);
+      if (paymentIntentData != null) {
+        await Stripe.instance.initPaymentSheet(
+            paymentSheetParameters: SetupPaymentSheetParameters(
+              applePay: PaymentSheetApplePay(merchantCountryCode: "+92"),
+              googlePay: PaymentSheetGooglePay(currencyCode: "UAD",merchantCountryCode: '+92',testEnv: true),
+              style: ThemeMode.dark,
+              merchantDisplayName: 'Prospects',
+              customerId: paymentIntentData!['customer'],
+              paymentIntentClientSecret: paymentIntentData!['client_secret'],
+              customerEphemeralKeySecret: paymentIntentData!['ephemeralKey'],
+            ));
+        await displayPaymentSheet();
+      }
+    } catch (e, s) {
+      print('exception:$e$s');
+    }
+  }
+
+  displayPaymentSheet() async {
+    try {
+      print('---------------------');
+      await Stripe.instance.presentPaymentSheet();
+      print('---------------------');
+      // Get.snackbar('Payment', 'Payment Successful',
+      //     snackPosition: SnackPosition.BOTTOM,
+      //     backgroundColor: Colors.green,
+      //     colorText: Colors.white,
+      //     margin: const EdgeInsets.all(10),
+      //     duration: const Duration(seconds: 2));
+    } on Exception catch (e) {
+      if (e is StripeException) {
+        print("Error from Stripe: ${e.error.localizedMessage}");
+      } else {
+        print("Unforeseen error: ${e}");
+      }
+    } catch (e) {
+      print("exception:$e");
+    }
+  }
+
+  //  Future<Map<String, dynamic>>
+  createPaymentIntent(String amount, String currency) async {
+    try {
+      Map<String, dynamic> body = {
+        'amount': calculateAmount(amount),
+        'currency': currency,
+        'payment_method_types[]': 'card'
+      };
+      var response = await http.post(
+          Uri.parse('https://api.stripe.com/v1/payment_intents'),
+          body: body,
+          headers: {
+            'Authorization': 'Bearer sk_test_51LQumxHIiDDpFMOxWxgQR2wEGKW6bFd9g7K5DKZmSihYxASP7FD7zFPElAb5qgBsezcf2AVoYClKXol6MRwnyvcm00UuYC3U3P',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          });
+      return jsonDecode(response.body);
+    } catch (err) {
+      print('err charging user: ${err.toString()}');
+    }
+  }
+
+  calculateAmount(String amount) {
+    final a = (int.parse(amount)) * 100;
+    return a.toString();
+  }
+}
